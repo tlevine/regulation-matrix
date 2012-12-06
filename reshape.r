2828c52e7f91e31ea6d4502301a6206ab0aa3f25
Thomas Levine <occurrence@thomaslevine.com>
2012-12-06 12:55:43 -0500

    take out category

--- reshape.r
+++ reshape.r
@@ -9,15 +9,17 @@ names(m)[1] <- 'id'
 categories <- m[is.na(m$id),][2]
 categories$Questions <- factor(as.character(categories$Questions))
 questions <- m[!is.na(m$id),]
-questions$category <- categories[nrow(categories),'Questions']
+questions$category <- NA
 
 category.ranges <- data.frame(
   # All except the last range, which I already set above
-  category = categories[1:(nrow(categories) - 1), 'Questions'],
-  before = as.numeric(row.names(categories)[(2:nrow(categories)) - 1]),
-  after = as.numeric(row.names(categories)[2:nrow(categories)])
+  category = categories[1:nrow(categories), 'Questions'],
+  before = as.numeric(row.names(categories)[1:nrow(categories)]),
+  after = c(as.numeric(row.names(categories)[2:nrow(categories)]), nrow(m))
 )
-d_ply(category.ranges, 'before', function(df){
-  .questions <- questions[seq(df[1,'before'] + 1, df[1,'after']-1)),]
-  questions[seq(df[1,'before'] + 1, df[1,'after']-1),'category'] <- df[1,'category']
+
+questions <- ddply(category.ranges, 'before', function(df){
+  .questions <- questions[seq(df[1,'before'] + 1, df[1,'after'] - 1),]
+  .questions$category <- df[1,'category']
+  .questions
 })
